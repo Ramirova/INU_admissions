52202fef3060605f53cb83d8a7b40d13acc5f49b
Rozaliya <rozaliya.amirova@gmail.com>
2019-04-20 16:23:45 +0300

    Permission settings
    Restricted acces to managers' methods for other users

--- app/managers_module/controllers.py
+++ app/managers_module/controllers.py
@@ -23,6 +23,7 @@ from flask_jwt_extended import (
 )
 
 from app.candidates_module.models import Candidate, TestsStates
+from app.candidates_module.controllers import get_token_info
 from app.staff_module.models import Staff_member
 from .models import Manager, Interview, db
 from sqlalchemy.exc import SQLAlchemyError
@@ -60,59 +61,71 @@ module = Blueprint('managers', __name__, url_prefix='/api/managers')
 @module.route('/updateStatus', methods=['POST'])
 @jwt_required
 def update_candidate_status():
-    candidate = Candidate.query.get(request.get_json().get('login'))
-    status = request.get_json().get('status')
-    if candidate:
-        candidate.status = status
-        db.session.commit()
+    login, role = get_token_info(request)
+    if role == 'manager':
+        candidate = Candidate.query.get(request.get_json().get('login'))
+        status = request.get_json().get('status')
+        if candidate:
+            candidate.status = status
+            db.session.commit()
 
-        if status == 'PASSING_TESTS':
-            test_states = TestsStates.query.filter_by(candidate=candidate.login).all()
-            for test_state in test_states:
-                test_state.permission = "granted"
-                db.session.commit()
+            if status == 'PASSING_TESTS':
+                test_states = TestsStates.query.filter_by(candidate=candidate.login).all()
+                for test_state in test_states:
+                    test_state.permission = "granted"
+                    db.session.commit()
 
-        return Response("Success", status=200, mimetype='application/json')
-    return Response("", status=401, mimetype='application/json')
+            return Response("Success", status=200, mimetype='application/json')
+        return Response("", status=401, mimetype='application/json')
+    else:
+        return Response("You do not have access rights", status=401, mimetype='application/json')
 
 
 @module.route('/users', methods=["GET"])
 @jwt_required
 def get_users():
-    role = request.get_json().get('role')
-    result_data = []
-    if role == 'candidate':
-        candidates = Candidate.query.all()
-        if request.get_json().get('status'):
-            candidates = Candidate.query.filter_by(state=request.get_json().get('status'))
-        for user in candidates:
-            result_data.append({
-                'login': user.login,
-                'name': user.name,
-                'surname': user.surname,
-                'second_name': user.second_name,
-                'date_of_birth': str(user.date_of_birth),
-                'status': user.state,
-                'nationality': user.nationality
-            })
-    elif role == 'staff_member':
-        for user in Staff_member.query.all():
-            result_data.append({
-                'login': user.login,
-                'name': user.name,
-                'surname': user.surname,
-                'second_name': user.second_name,
-            })
-    return make_response(jsonify(result_data)), 200
+    login, user_role = get_token_info(request)
+    if user_role == 'manager':
+        role = request.get_json().get('role')
+        result_data = []
+        if role == 'candidate':
+            candidates = Candidate.query.all()
+            if request.get_json().get('status'):
+                candidates = Candidate.query.filter_by(state=request.get_json().get('status'))
+            for user in candidates:
+                result_data.append({
+                    'login': user.login,
+                    'name': user.name,
+                    'surname': user.surname,
+                    'second_name': user.second_name,
+                    'date_of_birth': str(user.date_of_birth),
+                    'status': user.state,
+                    'nationality': user.nationality
+                })
+        elif role == 'staff_member':
+            for user in Staff_member.query.all():
+                result_data.append({
+                    'login': user.login,
+                    'name': user.name,
+                    'surname': user.surname,
+                    'second_name': user.second_name,
+                })
+        return make_response(jsonify(result_data)), 200
+    else:
+        return Response("You do not have access rights", status=401, mimetype='application/json')
 
 
 @module.route('/interview', methods=["POST"])
 @jwt_required
 def create_interview():
-    candidate = request.get_json().get('candidate')
-    staff_member = request.get_json().get('staff_member')
-    date = request.get_json().get('date')
-    interview = Interview(candidate, staff_member, date)
-    db.session.add(interview)
-    db.commit()
-    return make_response(""), 200
+    login, user_role = get_token_info(request)
+    if user_role == 'manager':
+        candidate = request.get_json().get('candidate')
+        staff_member = request.get_json().get('staff_member')
+        date = request.get_json().get('date')
+        interview = Interview(candidate, staff_member, date)
+        db.session.add(interview)
+        db.commit()
+        return make_response(""), 200
+    else:
+        return Response("You do not have access rights", status=401, mimetype='application/json')
